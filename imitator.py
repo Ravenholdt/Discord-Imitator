44a8e8cb2c7f15b0ca804806bafa28e223581738
Unknown <joppe4899@hotmail.com>
2017-09-25 01:06:01 +0200

    Adding commands for load and unload of extensions.

--- imitator.py
+++ imitator.py
@@ -24,6 +24,23 @@ async def on_ready():
     print('------')
 
 
+@bot.command()
+async def load(extension_name : str):
+    """Loads an extension."""
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
+
+@bot.command()
+async def unload(extension_name : str):
+    """Unloads an extension."""
+    bot.unload_extension(extension_name)
+    await bot.say("{} unloaded.".format(extension_name))
+
+
 @bot.command(pass_context=True)
 async def hello(ctx):
     """Bot says hello!"""
