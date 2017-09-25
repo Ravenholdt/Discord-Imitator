9a58fa499e011f788c78348aacc655a639485971
Unknown <joppe4899@hotmail.com>
2017-09-26 00:20:00 +0200

    Added support for Wikipedia and Urban Dictionary

--- info.py
+++ info.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import asyncio
 
 import wolframalpha
+import wikipedia
+import urbandictionary as ud
+
 import config
 
 client = wolframalpha.Client(config.wolframappid)
@@ -17,7 +20,26 @@ class Info:
         res = client.query(query)
         #res = 'Please ignore this function.'
         answer = next(res.results).text
-        await self.bot.say("From Wolfram Alpha:\n" + answer)
+        await self.bot.say("**From Wolfram Alpha:**\n" + answer)
+
+
+    @commands.command()
+    async def wikipedia(self, *, query : str):
+        """Search the Wikipedia."""
+        page = wikipedia.page(query)
+        summary = wikipedia.summary(query)
+        msg = "**"+page.title+"**\n"+summary+"\n"+page.url
+        await self.bot.say(msg)
+
+
+    @commands.command()
+    async def urban(self, *, query : str):
+        """Search the Urban Dictionary."""
+        msg = "**From Urban Dictionary:**\n"
+        defs = ud.define(query)
+        for d in defs:
+            msg = msg + "**" + defs[0].word + ":** " + d.definition + "\n"
+        await self.bot.say(msg)
 
 
 def setup(bot):
