8b5e9a940b2fea38d6e0cf1297499ab4f704fe7e
Unknown <joppe4899@hotmail.com>
2017-09-29 04:18:27 +0200

    Git added.

--- gitPull.py
+++ gitPull.py
@@ -1,14 +1,9 @@
 import os
-import git
 
 import config
 
-branch = "master"
+branch = ""
 if config.gitDev:
     branch = "development"
 
-#o = git.Repo.pull_from("https://github.com/Ravenholdt/Discord-Imitator.git" , os.path.dirname(os.path.abspath(__file__)), branch=branch)
-
-repo = git.Repo("https://github.com/Ravenholdt/Discord-Imitator.git")
-o = repo.remotes.origin.development
-o.fetch()
+os.system("git pull origin " + branch)
