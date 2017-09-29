6f01d4c2d99635f59568dec7fadb1d0b1fd1247d
Unknown <joppe4899@hotmail.com>
2017-09-29 03:30:47 +0200

    Removed pull.

--- gitPull.py
+++ gitPull.py
@@ -8,4 +8,3 @@ if config.gitDev:
     branch = "development"
 
 o = git.Repo.clone_from("https://github.com/Ravenholdt/Discord-Imitator.git" , os.path.dirname(os.path.abspath(__file__)), branch=branch)
-o.pull()
